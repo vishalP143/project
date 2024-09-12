bbe6cb272a918b7da87582a2e387438e8b655bd3
vishalP143 <vishal.p8070@gmail.com.com>
2024-09-12 10:41:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+const app = express();
+
+import bodyParser from 'body-parser'
+
+import userRoutes from './routes/users.js'
+const port = 5000;
+app.get("/",(req,res) => {
+    res.send ("Hello World!");
+})
+
+  
+
+// this is dummy api
+app.get("/api/v1",(req,res) => 
+{
+    res.json(
+        {
+            message: "Welcome to API"
+        }
+    )
+})
+
+//adding body parser for json
+app.use(bodyParser.json());
+
+app.use('/users',userRoutes);
+
+
+app.listen(port,() => {
+    console.log(`Server started on port ${port}`);
+})
