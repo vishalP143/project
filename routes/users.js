8fe6316402dc4e29a0efd3cc38e055d86158fb45
vishalP143 <vishal.p8070@gmail.com.com>
2024-09-12 15:50:54 +0530

    first commit

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,13 @@ const router = express.Router();
 //   },
 // ];
 
-const users = []
+// {
+//     "first_name": "Vishal",
+//     "last_name": "Pradhan",
+//     "email": "johndoe@example.com"
+//   }
+
+let users = []
 // Getting the list of users from the mock database
 router.get('/', (req, res) => {
     res.send(users);
@@ -30,4 +36,36 @@ router.post('/', (req, res) => {
 
     res.send(`${user.first_name} has been added to the Database`);
 })
+
+//
+router.get('/:id', (req, res) => {
+    const {id} =  req.params;
+    const founduser = users.find((user) => user.id === id)
+    res.send(founduser);
+    });
+
+    // delete /:id - delete a specific user by their id
+    router.delete('/:id', (req, res) => {
+        const { id } = req.params;
+
+        users = users.filter((user) => user.id !== id)
+        res.send(`User with id ${id} has been deleted from the database`
+            );
+            });
+
+             // update /:id - update a specific user by their id
+             router.put('/:id', (req, res) => {
+                const { id } = req.params;
+
+                const { first_name, last_name, email } = req.body;
+
+                const user = users.find((user) => user.id === id)
+
+                if(first_name) user.first_name =  first_name;
+                if(last_name) user.last_name = last_name;
+                if(email) user.email = email;
+
+                res.send(`User with id ${id} has been updated in the database`)
+                });
+
 export default router
